c92352ab4f2f55afbd56f674562657a9dea0f546
petersonbabs <timiebabs@gmail.com>
2024-11-19 08:43:46 +0100

    dev

---
+++ script.js
@@ -0,0 +1,6 @@
+
+// Create a counterEl variable (to store the counterEl element where the number of students are being displayed)
+// Create a IncrementBtn variable 
+// Create a decrementBtn variable 
+// Create a function to increment the numberOfStudents and update the textContent of the counterEl
+// Create a function to decrement the numberOfStudents and update the textContent of the counterEl
